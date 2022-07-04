c8cd7b43cbaf7ebf43934b3ea469b9638194189e
HECTOR JAVIER LECAROS GONZALEZ <hector.lecaros@inacapmail.cl>
2022-07-04 16:08:41 -0400

    implementacion de DAL

---
+++ EventosModel/DAL/AsistentesDALDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosModel.DAL
+{
+    public class AsistentesDALDB : IAsistentesDAL
+    {
+
+        private EventosDBEntities EventosDB = new EventosDBEntities();
+        public void AgregarAsistente(Asistente asistente)
+        {
+            this.EventosDB.Asistentes.Add(asistente);
+            this.EventosDB.SaveChanges();
+        }
+
+        public void EliminarAsistente(int id)
+        {
+            //buscar asistente con el id
+            Asistente asistente = this.EventosDB.Asistentes.Find(id);
+            //eliminar el asistente
+            this.EventosDB.Asistentes.Remove(asistente);
+            this.EventosDB.SaveChanges();
+        }
+
+        public Asistente Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Asistente> ObtenerAsistentes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Asistente> ObtenerAsistentes(string estado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EventosModel/DAL/RegionesDALDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosModel.DAL
+{
+    public class RegionesDALDB : IRegionesDAL
+    {
+        //intanciar la base de datos
+        private EventosDBEntities EventosDB = new EventosDBEntities();
+
+        //retornar los datos
+        public List<Region> ObtenerRegiones()
+        {
+            return this.EventosDB.Regions.ToList();
+        }
+    }
+}
